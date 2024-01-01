14b2bc76b57d6ecd3aaf4d75c1ca87c0b07a4b74
raghu4sangeeth <34170707+raghu4sangeeth@users.noreply.github.com>
2024-01-01 13:12:41 +0300

    Add files via upload

---
+++ AssigntoPremium.ps1
@@ -0,0 +1,16 @@
+﻿$body       = '{capacityId:"xxx-xxxx-xxx-xxx"}'
+$workspaces = Import-Csv -Path D:\WorkSpaces.csv -Delimiter ","
+#Log in to service
+Connect-PowerBIServiceAccount
+
+$workspaces | ForEach-Object {
+    
+    $WorkspaceID = $_.Id
+    $url = "groups/$WorkspaceID/AssignToCapacity"
+
+    Write-Host $url
+    Write-Host $body
+
+    $re = Invoke-PowerBIRestMethod -Url $Url -Method Post -Body $body
+    $re
+}
